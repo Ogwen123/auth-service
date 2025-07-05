687ee3de48b9d78213f7d1806290a0641ec2a85f
Owen Jones <owen.d.jones@btinternet.com>
2025-07-05 14:16:01 +0100

    change to express 5 syntax

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const port = 3000
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/*', (req, res, next) => {
+app.use('(.*)', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization,Content-Length");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,PATCH,DELETE,OPTIONS")
